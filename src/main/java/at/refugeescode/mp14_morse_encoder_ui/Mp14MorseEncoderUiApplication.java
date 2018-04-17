98e9ad53b6c66bb7a0c2cfcf20de779f02956b08
Fozat Kardouh <fozatkardouh@gmail.com>
2018-04-17 14:34:05 +0200

    added empty project

---
+++ src/main/java/at/refugeescode/mp14_morse_encoder_ui/Mp14MorseEncoderUiApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp14_morse_encoder_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp14MorseEncoderUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp14MorseEncoderUiApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp14_morse_encoder_ui/Mp14MorseEncoderUiApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp14_morse_encoder_ui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp14MorseEncoderUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
